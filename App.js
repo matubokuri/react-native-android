2b93088496dcca56d0c362af9ace5294a5ed3849
Koichi Matsuda <k-matsuda@caldia.org>
2018-03-20 14:49:50 +0900

    State
    https://facebook.github.io/react-native/docs/state.html

--- App.js
+++ App.js
@@ -4,23 +4,37 @@
  * @flow
  */
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { AppRegistry, Text, View } from 'react-native';
 
-class Greeting extends Component<{ name: string }, {}> {
+class Blink extends Component<{ text: string }, { isShowingText: boolean }> {
+  constructor(props) {
+    super(props);
+    this.state = { isShowingText: true };
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
   render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
     return (
-      <Text>Hello {this.props.name}!</Text>
+      <Text>{display}</Text>
     );
   }
 }
 
-export default class LotsOfGreetings extends Component<{}, {}> {
+export default class BlinkApp extends Component<void, void> {
   render() {
     return (
-      <View style={{ alignItems: 'center' }}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jania' />
-        <Greeting name='Valeera' />
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
