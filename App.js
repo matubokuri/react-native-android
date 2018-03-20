843a5a279ed89cba08f35cad7adc723bc40f0870
Koichi Matsuda <k-matsuda@caldia.org>
2018-03-20 15:41:37 +0900

    Layout with Flexbox - Flex Direction - column

--- App.js
+++ App.js
@@ -10,10 +10,10 @@ export default class FlexDirectionBasics extends Component<void, void> {
   render() {
     return (
       // Try setting `flexDirection` to `column`.
-      <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      <View style={{ flex: 1, flexDirection: 'column' }}>
+        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+        <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
+        <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
       </View>
     );
   }
