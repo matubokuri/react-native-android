8c52032071c12f0c6bb95fa332f156954537a28c
Koichi Matsuda <k-matsuda@caldia.org>
2018-03-20 15:49:45 +0900

    Layout with Flexbox -  Justify Content
    https://facebook.github.io/react-native/docs/flexbox.html

--- App.js
+++ App.js
@@ -6,11 +6,16 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
-export default class FlexDirectionBasics extends Component<void, void> {
+export default class JustifyContentBasics extends Component<void, void> {
   render() {
     return (
-      // Try setting `flexDirection` to `column`.
-      <View style={{ flex: 1, flexDirection: 'column' }}>
+      // Try setting `justifyContent` to `center`.
+      // Try setting `flexDirection` to `row`.
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
         <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
         <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
         <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
