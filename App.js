ccc1331d1653f8db8a2d00fd4397251e1c253f74
Koichi Matsuda <k-matsuda@caldia.org>
2018-03-20 16:05:44 +0900

    Handling Touches - Displaying a basic button
    https://facebook.github.io/react-native/docs/handling-touches.html

--- App.js
+++ App.js
@@ -4,26 +4,56 @@
  * @flow
  */
 import React, { Component } from 'react';
-import { View, TextInput, Text } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
 
-export default class PizzaTranslator extends Component<void, {text: string}> {
-  constructor() {
-    super();
-    this.state = {text: ''};
+export default class ButtonBasics extends Component<void, void> {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
   }
 
   render() {
     return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title="OK!"
+            color="#841584"
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
