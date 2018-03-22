7966de8e877755156e3dd022cb26de3c878fd57b
Koichi Matsuda <k-matsuda@caldia.org>
2018-03-22 10:54:52 +0900

    Using List Views - FlatList Basics
    https://facebook.github.io/react-native/docs/using-a-listview.html

--- App.js
+++ App.js
@@ -4,44 +4,38 @@
  * @flow
  */
 import React, { Component } from 'react';
-import { ScrollView, Image, Text } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component<void, void> {
+export default class FlatListBasics extends Component<void, void> {
   render() {
-      return (
-        <ScrollView>
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Text style={{fontSize:96}}>If you like</Text>
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Text style={{fontSize:96}}>Scrolling down</Text>
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Text style={{fontSize:96}}>What's the best</Text>
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Text style={{fontSize:96}}>Framework around?</Text>
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Image source={require('./img/favicon.png')} />
-          <Text style={{fontSize:80}}>React Native</Text>
-        </ScrollView>
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
