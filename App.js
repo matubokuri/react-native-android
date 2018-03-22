467467efa74181138dc0320b979dcfafd2519e72
Koichi Matsuda <k-matsuda@caldia.org>
2018-03-22 11:06:35 +0900

    Using List Views - SectionList
    https://facebook.github.io/react-native/docs/using-a-listview.html

--- App.js
+++ App.js
@@ -4,24 +4,20 @@
  * @flow
  */
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { SectionList, StyleSheet, Text, View } from 'react-native';
 
-export default class FlatListBasics extends Component<void, void> {
+export default class SectionListBasics extends Component<void, void> {
   render() {
     return (
       <View style={styles.container}>
-        <FlatList
-          data={[
-            {key: 'Devin'},
-            {key: 'Jackson'},
-            {key: 'James'},
-            {key: 'Joel'},
-            {key: 'John'},
-            {key: 'Jillian'},
-            {key: 'Jimmy'},
-            {key: 'Julie'},
+        <SectionList
+          sections={[
+            {title: 'D', data: ['Devin']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
           ]}
-          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item, index) => index.toString()}
         />
       </View>
     );
@@ -32,6 +28,15 @@ const styles = StyleSheet.create({
   container: {
    flex: 1,
    paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 5,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(200,200,200,1.0)',
   },
   item: {
     padding: 10,
