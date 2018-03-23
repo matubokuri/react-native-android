5dd5d0b2b2427e6a9224cf71c800a7e2a97dfb31
Koichi Matsuda <k-matsuda@caldia.org>
2018-03-23 11:31:34 +0900

    Add test

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,12 +1,50 @@
 import 'react-native';
 import React from 'react';
+import { configure, shallow } from 'enzyme';
 import App from '../App';
+import Adapter from 'enzyme-adapter-react-16';
 
+configure({ adapter: new Adapter() });
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer.create(
+const dataSource = {
+  "title": "The Basics - Networking",
+  "description": "Your app fetched this from a remote endpoint!",
+  "movies": [
+    { "title": "Star Wars", "releaseYear": "1977" },
+    { "title": "Back to the Future", "releaseYear": "1985" },
+    { "title": "The Matrix", "releaseYear": "1999" },
+    { "title": "Inception", "releaseYear": "2010" },
+    { "title": "Interstellar", "releaseYear": "2014" }
+  ]
+}
+
+fetch = jest.fn((url: string) => {
+  return new Promise((resolve, reject) => {
+    const value = {
+      json: () => {
+        return dataSource
+      }
+    }
+    process.nextTick(() => resolve(value))
+  })
+})
+
+it('renders correctly', async () => {
+  expect.assertions(5)
+  const tree = shallow(
     <App />
   );
+  expect(tree.state('isLoading')).toBe(true)
+  expect(tree.state('dataSource')).toBeUndefined()
+
+  await tree.instance().componentDidMount()
+
+  expect(tree.state('isLoading')).toBe(false)
+  expect(tree.state('dataSource')).toHaveLength(dataSource.movies.length)
+
+  tree.setState({ 'dataSource': dataSource.movies })
+
+  expect(tree).toMatchSnapshot()
 });
